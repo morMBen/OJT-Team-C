8d9ee357cab5aa1f54eb46a4a7456e0c029a50d4
Mor Mordechai Ben <31767946+morMBen@users.noreply.github.com>
2022-03-17 14:57:28 +0200

    app.tsx example to the spacieal component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,43 @@
-import './App.css';
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Redirect,
+// } from 'react-router-dom';
+// import SpecialRoute from './specialRoute/SpecialRoute';
+
+// import TempLoginPage from './TempLoginPage/TempLoginPage';
+// import TempPage from './TempLoginPage/TempPage';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      {/* <Router>
+        <Switch>
+          <Route exact path='/login'>
+            <SpecialRoute>
+              <TempLoginPage />
+              <Redirect to='/' />
+            </SpecialRoute>
+          </Route>
+
+          <Route exact path='/'>
+            <SpecialRoute>
+              <Redirect to='/login' />
+              <TempPage pageName='Home' />
+            </SpecialRoute>
+          </Route>
+
+          <Route exact path='/todo'>
+            <SpecialRoute>
+              <Redirect to='/login' />
+              <TempPage pageName='Todo' />
+            </SpecialRoute>
+          </Route>
+        </Switch>
+      </Router> */}
+    </>
+  );
 }
 
 export default App;
