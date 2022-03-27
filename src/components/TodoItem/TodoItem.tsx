62a721cea2d36b024026c10297c53982a4963bdc
Mor Mordechai Ben <31767946+morMBen@users.noreply.github.com>
2022-03-28 00:56:46 +0300

    todo items 3 dots manu styling

--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -1,6 +1,7 @@
-import React from "react";
-import { Todo } from "../../model";
-import "./TodoItem.css";
+import React from 'react';
+import { Dropdown, DropdownButton } from 'react-bootstrap';
+import { Todo } from '../../model';
+import './TodoItem.css';
 
 interface Props {
   todo: Todo;
@@ -9,5 +10,18 @@ interface Props {
 }
 
 export const TodoItem: React.FC<Props> = ({ todo, todos, setTodos }) => {
-  return <div className="todoItem">{todo.todo}</div>;
+  const handleEdit = () => {};
+  return (
+    <div className='todoItem'>
+      {todo.todo}
+      <DropdownButton align='end' title='' id='dropdown-menu-align-end'>
+        <Dropdown.Item eventKey='1' onClick={handleEdit}>
+          Edit
+        </Dropdown.Item>
+        <Dropdown.Item eventKey='2'>Delete</Dropdown.Item>
+        <Dropdown.Divider />
+        <Dropdown.Item eventKey='3'>Share </Dropdown.Item>
+      </DropdownButton>
+    </div>
+  );
 };
