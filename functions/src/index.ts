dab0d551b7765b34898df013baaa0394846c7160
Or Samucian <orfacemailj@gmail.com>
2022-03-31 22:16:04 +0300

    cloud function get user

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,136 @@
-import * as functions from "firebase-functions";
+import { Request, Response } from "express";
 import * as express from "express";
-import {createUser, getUser, updateUser} from "./userController";
-import {createTodo, getTodos, updateTodo, deleteTodo} from "./todoController";
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import StatusCodes from "http-status-codes";
 
+//////////////////////////
+admin.initializeApp();
+//////////////////////////
+const db = admin.firestore();
 const app = express();
+//////////////////////////
 
+const createUser = (req: Request, res: Response) => {
+  try {
+    const { email, userName, password } = req.body;
+
+    if (!email || !userName || !password) {
+      const result = {
+        status: "failed",
+        reason: "missing fields",
+      };
+      res.status(StatusCodes.BAD_REQUEST);
+      res.send(JSON.stringify(result));
+      return;
+    }
+    const user = {
+      newToApp: true,
+    };
+
+    await db.collection("users").add(user);
+    res.status(200);
+    res.send("createUser response");
+  } catch (error) {
+    res.status(500);
+    res.send(error);
+  }
+};
+
+const getUser = async (req: Request, res: Response) => {
+  try {
+    const userId = req.params.id;
+    const userSnapshot = await db.collection("users").doc(userId).get();
+    const isUserExist = userSnapshot.exists;
+
+    if (!isUserExist) {
+      const result = {
+        status: "failed",
+        reason: "user ID isn't found",
+      };
+      res.status(StatusCodes.NOT_FOUND);
+      res.send(JSON.stringify(result));
+      return;
+    }
+
+    const userData = userSnapshot.data();
+    const user = { id: userId, ...userData };
+
+    res.status(200);
+    res.send(JSON.stringify(user));
+  } catch (error) {
+    res.status(500);
+    res.send(error);
+  }
+};
+
+const getUsers = async (req: Request, res: Response) => {
+  try {
+    const usersSnapshot = await db.collection("users").get();
+    console.log(`users snapshot: ${JSON.stringify(usersSnapshot)}`);
+
+    const usersDocs = usersSnapshot.docs;
+    console.log(`users docs: ${JSON.stringify(usersDocs)}`);
+
+    const users: any[] = [];
+
+    usersDocs.forEach((doc) => {
+      const userId = doc.id;
+      const userData = doc.data();
+
+      users.push({ userId, ...userData });
+    });
+
+    res.status(200);
+    res.send(JSON.stringify(users));
+  } catch (error) {}
+};
+
+const updateUser = (req: Request, res: Response) => {
+  try {
+    res.status(200);
+    res.send("updateUser response");
+  } catch (error) {
+    res.status(500);
+    res.send(error);
+  }
+};
+
+//////////////////////////
+//////////////////////////
+const createTodo = (req: Request, res: Response) => {
+  try {
+    res.status(200);
+    res.send("create a todo 2");
+  } catch (error) {}
+};
+
+const getTodos = async (req: Request, res: Response) => {
+  try {
+    res.status(200);
+    res.send("getTodos response");
+  } catch (error) {
+    res.status(500);
+    res.send(error);
+  }
+};
+
+const updateTodo = (req: Request, res: Response) => {
+  try {
+    res.status(200);
+    res.send("update a todo 2");
+  } catch (error) {}
+};
+
+const deleteTodo = (req: Request, res: Response) => {
+  try {
+    res.status(200);
+    res.send("delete a todo 2");
+  } catch (error) {}
+};
+//////////////////////////
 app.post("/create-user", createUser);
+app.get("/get-users", getUsers);
 app.get("/get-user/:id", getUser);
 app.put("/update-user/:id", updateUser);
 
@@ -14,4 +139,4 @@ app.get("/get-todos/:id", getTodos);
 app.put("/update-todo/:id", updateTodo);
 app.delete("/delete-todo/:id", deleteTodo);
 
-exports.app = functions.https.onRequest(app);
+exports.api = functions.https.onRequest(app);
--- functions/src/todos/index.ts
+++
@@ -1,5 +0,0 @@
-// CRUD:
-// 1. Create - Create a new todo
-// 2. Read - Get all user's todo
-// 3. Update - Edit todo by ID
-// 4. Delete - Delete todo
