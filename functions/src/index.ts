69e5b93fbf811da957e6cb0f1f60fd032d5a3434
Or Samucian <orfacemailj@gmail.com>
2022-04-01 16:12:13 +0300

    update user

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,7 @@ import * as express from "express";
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 import StatusCodes from "http-status-codes";
+import { user } from "firebase-functions/v1/auth";
 
 //////////////////////////
 admin.initializeApp();
@@ -20,7 +21,7 @@ const createUser = async (req: Request, res: Response) => {
     if (!email || !userName || !password) {
       const result = {
         status: "failed",
-        reason: "missing fields",
+        message: "missing fields",
       };
       res.status(StatusCodes.BAD_REQUEST);
       res.send(JSON.stringify(result));
@@ -33,12 +34,12 @@ const createUser = async (req: Request, res: Response) => {
       password: password,
       appColor: null,
       newToApp: true,
+      todos: [{checked: false, text: "example"}],
     };
-
-    await db.collection("users").add(user);
+    const userSnapshot = await db.collection("users").add(user);
     const result = {
         status: "succeed",
-        reason: "user has been created successfully",
+        message: `user - ${userSnapshot.id} has been created successfully`,
       };
 
     res.status(200);
@@ -58,7 +59,7 @@ const getUser = async (req: Request, res: Response) => {
     if (!isUserExist) {
       const result = {
         status: "failed",
-        reason: "user ID isn't found",
+        message: "user ID isn't found",
       };
       res.status(StatusCodes.NOT_FOUND);
       res.send(JSON.stringify(result));
@@ -98,10 +99,22 @@ const getUsers = async (req: Request, res: Response) => {
   } catch (error) {}
 };
 
-const updateUser = (req: Request, res: Response) => {
+const updateUser = async (req: Request, res: Response) => {
   try {
+    // get user
+    const userId = req.params.id;
+    // if user isn't exist return an error
+    // create an update image of the user
+    const updateInfo = req.body;
+    // update the user in the db
+    await db.collection("users").doc(userId).update(updateInfo);
+    // return success
+    const result = {
+      status: "succeed",
+      message: "user info has been updated successfully",
+    };
     res.status(200);
-    res.send("updateUser response");
+    res.send(JSON.stringify(result));
   } catch (error) {
     res.status(500);
     res.send(error);
@@ -110,8 +123,15 @@ const updateUser = (req: Request, res: Response) => {
 
 //////////////////////////
 //////////////////////////
-const createTodo = (req: Request, res: Response) => {
+const createTodo = async (req: Request, res: Response) => {
   try {
+    const userId = req.params.id;
+    const {todo} = req.body;
+    const userTodos = await db.collection("users").doc(userId).get();
+    
+    userTodos.todos.push(todo);
+    await db.collection("users").doc(userId).update(userTodos);
+
     res.status(200);
     res.send("create a todo 2");
   } catch (error) {}
@@ -141,12 +161,23 @@ const deleteTodo = (req: Request, res: Response) => {
   } catch (error) {}
 };
 //////////////////////////
+// users crud
+// create - 
+// read - 
+// update - 
+
+// todos crud
+// create - 
+// read - 
+// update - 
+// delete - 
+
+//////////////////////////
 app.post("/create-user", createUser);
-app.get("/get-users", getUsers);
 app.get("/get-user/:id", getUser);
 app.put("/update-user/:id", updateUser);
 
-app.post("/create-todo", createTodo);
+app.post("/create-todo/:id", createTodo);
 app.get("/get-todos/:id", getTodos);
 app.put("/update-todo/:id", updateTodo);
 app.delete("/delete-todo/:id", deleteTodo);
